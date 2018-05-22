f0fa3dfb58d4591d0ee05375208bc81d8a81bed9
rjramos70 <rjramos70@gmail.com>
2018-05-22 15:40:37 -0300

    STD project created

---
+++ src/main/java/com/skipthedishes/api/SkipTheDishesApplication.java
@@ -0,0 +1,12 @@
+package com.skipthedishes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipTheDishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipTheDishesApplication.class, args);
+	}
+}
